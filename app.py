d80bfc6ace286e3176604ed8e8a9c8a22a3eaeda
丁丁 <Kutinging@users.noreply.github.com>
2018-10-24 14:56:53 +0800

    新增字幕組ID及名稱;修改route方法

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import re
 
 import aiohttp
 import requests
@@ -7,21 +8,6 @@ from flask import Flask, jsonify, make_response, request
 
 from modules.mods import *
 
-
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(website.table('https://share.dmhy.org/topics/list?keyword=&sort_id=2&team_id=117&order=date-desc'))
-"""
-tr_list = dmhy.animesearch('黑色五葉草','117')
-title_list = website.title(tr_list)
-magnet_list = website.magnet(tr_list)
-anime = {}
-
-
-for index in range(len(tr_list)):
-    anime[index] = {}
-    anime[index]['anime_title'] = title_list[index]
-    anime[index]['download_magnet'] = magnet_list[index]
-"""
 app = Flask(__name__)
 
 @app.route(f"/v{api.version}")
@@ -31,20 +17,46 @@ def home():
 
 @app.route(f'/v{api.version}/', defaults={'anime_title': None})
 @app.route(f"/v{api.version}/<string:anime_title>")
-@app.route(f'/v{api.version}/<string:anime_title>', defaults={'team_id': None})
-@app.route(f"/v{api.version}/<string:anime_title>/<int:team_id>")
-def anime_search(anime_title,team_id):
+def anime_search(anime_title='',team_id='',page=1,lang=''):
     """ search anime """
-    tr_list = dmhy.animesearch(anime_title,team_id)
-    title_list = website.title(tr_list)
-    magnet_list = website.magnet(tr_list)
-    anime = {}
-    for index in range(len(tr_list)):
-        anime[index] = {}
-        anime[index]['anime_title'] = title_list[index]
-        anime[index]['download_magnet'] = magnet_list[index]
-    return http.status(anime, 200)
+    try:
+        if request.args.get('page'):
+            page = request.args.get('page')
+        if request.args.get('lang'):
+            lang = request.args.get('lang')
+        if request.args.get('team'):
+            team_id = request.args.get('team')
+        tr_list = dmhy.animesearch(anime_title,team_id,page,lang)
+        title_list = website.title(tr_list)
+        magnet_list = website.magnet(tr_list)
+        teamid_list = website.teamid(tr_list)
+        teamname_list = website.teamname(tr_list)
+        anime = {}
+        for index in range(len(tr_list)):
+            anime[index] = {}
+            anime[index]['anime_title'] = title_list[index]
+            anime[index]['anime_Fansub_team_id'] = teamid_list[index]
+            anime[index]['anime_Fansub_team_name'] = teamname_list[index]
+            anime[index]['download_magnet'] = magnet_list[index]
+        return http.status(anime, 200)
+    except TypeError as e:
+        log.write(str(e))
+        return http.not_found(f'第 {page} 頁無資料')
+    except Exception as e:
+        log.write(str(e))
+        return http.internal_server_error(str(e))
+
+
+@app.errorhandler(404)
+def _not_found(e):
+    """ 404 not found"""
+    return http.not_found('404 not found')
+
+@app.errorhandler(500)
+def _internal_server_error(e):
+    """ Internal Server Error """
+    return http.internal_server_error('Server Error')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=7000)
--- modules/mods.py
+++ modules/mods.py
@@ -1,10 +1,11 @@
 import asyncio
+import re
 import time
 
 import aiohttp
 import requests
 from bs4 import BeautifulSoup
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 
 from config import *
 
@@ -19,40 +20,70 @@ class website:
             return htmlcode
         except Exception as e:
             log.write(f'>> website.html : {str(e)}')
-        """
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                html = await resp.text()
-                htmlcode = BeautifulSoup(html, "html.parser")
-                return htmlcode
-        """
+    
     @staticmethod
     def tr(url):
-        htmlcode = website.html(url)
-        tbody = htmlcode.find('tbody')
-        tr_list = tbody.find_all('tr')
-        return tr_list
+        try:
+            htmlcode = website.html(url)
+            tbody = htmlcode.find('tbody')
+            tr_list = tbody.find_all('tr')
+            return tr_list
+        except Exception as e:
+            log.write(f'>> website.tr : {str(e)}')
 
     @staticmethod
     def title(tr_list):
-        title_list = []
-        for tr in tr_list:
-            title_list.append((tr.find('a',{'target':'_blank'}).text).replace('\n\t\t\t\t',''))
-        return title_list
+        try:
+            title_list = []
+            for tr in tr_list:
+                title_list.append((tr.find('a',{'target':'_blank'}).text).replace('\n\t\t\t\t',''))
+            return title_list
+        except Exception as e:
+            log.write(f'>> website.title : {str(e)}')
+
+    @staticmethod
+    def teamname(tr_list):
+        try:
+            teamname_list = []
+            for tr in tr_list:
+                teamname_list.append(((tr.find('span',{'class':'tag'})).text).replace('\n\n\t\t\t\t',''))
+            return teamname_list
+        except Exception as e:
+            log.write(f'>> website.teamname : {str(e)}')
+
+    @staticmethod
+    def teamid(tr_list):
+        try:
+            teamid_list = []
+            for tr in tr_list:
+                teamid_list.append((re.findall(r'team_id/\d+',str(tr))[0]).split('/')[1])
+            return teamid_list
+        except Exception as e:
+            log.write(f'>> website.teamid : {str(e)}')
 
     @staticmethod
     def magnet(tr_list):
-        magnet_list = []
-        for tr in tr_list:
-            magnet_list.append((tr.find('a',{'class':'download-arrow arrow-magnet'})['href']))
-        return magnet_list
+        try:
+            magnet_list = []
+            for tr in tr_list:
+                magnet_list.append((tr.find('a',{'class':'download-arrow arrow-magnet'})['href']))
+            return magnet_list
+        except Exception as e:
+            log.write(f'>> website.magnet : {str(e)}')
 
 
 class dmhy:
     @staticmethod
-    def animesearch(keyword=None,team_id=None):
-        return website.tr(f'https://share.dmhy.org/topics/list?keyword={keyword}&sort_id=2&team_id={team_id}&order=date-desc')
-
+    def animesearch(keyword=None,team_id=None,page=1,lang=None):
+        """
+        # Search anime from dmhy
+        # dmhy.animesearch(keyword=None,team_id=None,page=1)
+        """
+        try:
+            print(f'https://share.dmhy.org/topics/list/page/{page}?keyword={keyword}+{lang}&sort_id=2&team_id={team_id}&order=date-desc')
+            return website.tr(f'https://share.dmhy.org/topics/list/page/{page}?keyword={keyword}+{lang}&sort_id=2&team_id={team_id}&order=date-desc')
+        except Exception as e:
+            return log.write(f'>> dmhy.animesearch : {str(e)}')
 
 class log:
     @staticmethod
@@ -61,14 +92,34 @@ class log:
 
     @staticmethod
     def write(message):
-         with open( f'{config.appname}.log','a',encoding='UTF-8') as log:
-            log.write(log._get_time()+' '+message+'\n')
+         with open( f'{config.appname}.log','a',encoding='UTF-8') as logfile:
+            logfile.write(log._get_time()+' '+message+'\n')
 
 
 class http:
     @staticmethod
     def status(message, status_code):
+        http.req(status_code)
         return make_response(jsonify(message), status_code)
+
+    @staticmethod
+    def req(status_code):
+        with open( f'{config.appname}.req.log','a',encoding='UTF-8') as logfile:
+            message = f'{request.headers.get("X-Real-Ip", request.remote_addr)} - - {log._get_time()} {request.method} {request.path}'
+            if request.args.get("page") or request.args.get("lang") or request.args.get("team"):
+                message += f'?page={request.args.get("page")}&lang={request.args.get("lang")}&team={request.args.get("team")}'
+            message += f' {request.environ.get("SERVER_PROTOCOL")} {status_code} -\n'
+            return logfile.write(message)
+
+    @staticmethod
+    def not_found(e):
+        """ 404 not found"""
+        return http.status({'message':str(e),'status':'404'},404)
+    
+    @staticmethod
+    def internal_server_error(e):
+        """ Internal Server Error """
+        return http.status({'message':str(e),'status':'500'},500)
 
 class api:
     version = '1'
